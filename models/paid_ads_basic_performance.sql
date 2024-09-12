25d46019ca30991543edc7f71c5a4c1fda642250
Damir <savezhanov@gmail.com>
2024-09-12 13:11:21 +0000

    union all stg models in paid_ads_basic_performance

--- models/paid_ads_basic_performance.sql
+++ models/paid_ads_basic_performance.sql
@@ -1,44 +1,24 @@
 WITH ads_bing_all_data AS (
-    SELECT 
-    ad_id
-    add_to_cart
-    adset_id
-    campaign_id
-    channel
-    clicks
-    comments
-    creative_id
-    date
-    engagements
-    impressions
-    installs
-    likes
-    link_clicks
-    placement_id
-    post_click_conversions
-    post_view_conversions
-    posts
-    purchase
-    registrations
-    revenue
-    shares
-    spend
-    total_conversions
-    video_views
-    FROM {{ ref('src_ads_bing_all_data') }}
+    SELECT *
+    FROM {{ ref('stg_ads_bing_all_data') }}
 ),
 
 ads_creative_facebook_all_data AS (
     SELECT *
-    FROM {{ ref('src_ads_creative_facebook_all_data') }}
+    FROM {{ ref('stg_ads_creative_facebook_all_data') }}
 ),
 
 ads_tiktok_ads_all_data AS (
     SELECT *
-    FROM {{ ref('src_ads_tiktok_ads_all_data') }}
+    FROM {{ ref('stg_ads_tiktok_ads_all_data') }}
 ),
 
-seed_paid_ads AS (
+promoted_tweets_twitter_all_data AS (
+    SELECT *
+    FROM {{ ref('stg_promoted_tweets_twitter_all_data') }}
+),
+
+paid_ads_basic_performance AS (
     SELECT *
     FROM ads_bing_all_data
     UNION ALL
@@ -47,6 +27,9 @@ seed_paid_ads AS (
     UNION ALL
     SELECT *
     FROM ads_tiktok_ads_all_data
+    UNION ALL
+    SELECT *
+    FROM promoted_tweets_twitter_all_data
 )
 
-SELECT * FROM seed_paid_ads
+SELECT * FROM paid_ads_basic_performance
